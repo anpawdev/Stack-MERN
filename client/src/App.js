cafb281bc4f1cb14fd98516bc7570dfa699f14f6
Anna Pawlik <pawlikanna24@gmail.com>
2019-12-04 12:11:41 +0100

    Add route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Home from './components/pages/Home/HomePage';
 import Posts from './components/pages/Posts/PostsPage';
 import Contact from './components/pages/Contact/ContactPage';
 import NotFound from './components/pages/NotFound/NotFoundPage';
+import AddPost from './components/pages/AddPost/AddPostPage';
+import SinglePost from './components/pages/SinglePost/SinglePostPage';
 
 class App extends React.Component {
   render() {
@@ -12,9 +14,11 @@ class App extends React.Component {
       <MainLayout>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/posts/new" exact component={AddPost} />
+          <Route path="/posts/:id" exact component={SinglePost} />
           <Route path="/posts" exact component={Posts} />
           <Route path="/contact" exact component={Contact} />
-          <Route component={NotFound} /> 
+          <Route component={NotFound} />
         </Switch>
       </MainLayout>
     );
