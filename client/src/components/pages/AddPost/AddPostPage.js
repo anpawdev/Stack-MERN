4da8969cdff0806abce7849495e383f114160e53
Anna Pawlik <pawlikanna24@gmail.com>
2019-12-16 12:33:16 +0100

    Update AddPost

--- client/src/components/pages/AddPost/AddPostPage.js
+++ client/src/components/pages/AddPost/AddPostPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import PageTitle from '../../common/PageTitle/PageTitle';
+import PostForm from '../../features/PostForm/PostFormContainer';
 
 const AddPostPage = () => (
     <div>
-      <h1>Add Post</h1>
+      <PageTitle>Add post</PageTitle>
+      <PostForm />
     </div>
   );
   
