1be9c5d63082e80fdb46f02c5de0918f3d23f0ee
Anna Pawlik <pawlikanna24@gmail.com>
2019-12-18 11:51:26 +0100

    Prepare app to publish

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const loadTestData = require('./testData');
 const helmet = require('helmet');
 const sanitize = require('mongo-sanitize');
+const path = require('path');
 
 const app = express();
 
@@ -17,6 +18,12 @@ app.use(express.json());
 app.use(express.json(sanitize()));
 app.use('/api', postRoutes);
 app.use(helmet());
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+});
 
 // connects our back end code with the database
 mongoose.connect(config.DB, { useNewUrlParser: true });
