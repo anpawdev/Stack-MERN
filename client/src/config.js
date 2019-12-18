d55ca7ba8200c9f4ad26e04ab6ea49c6b7c0afd3
Anna Pawlik <pawlikanna24@gmail.com>
2019-12-18 15:02:04 +0100

    Update

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://blogappmern.herokuapp.com/api';
-export const BASE_URL = 'https://blogappmern.herokuapp.com/';
+export const API_URL = 'http://localhost:8000/api';
+export const BASE_URL = 'http://localhost:3000';
