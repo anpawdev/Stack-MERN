2b6c34258469f16a1e32384073676f1f06c49ca7
Anna Pawlik <pawlikanna24@gmail.com>
2019-12-18 12:34:19 +0100

    Configuration Variable

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PORT: 8000,
-    DB: 'mongodb+srv://User_app:testapp12blog@cluster0-lnujk.mongodb.net/test?retryWrites=true&w=majority',
+    PORT: process.env.PORT,
+    DB: process.env.DB,
 };
