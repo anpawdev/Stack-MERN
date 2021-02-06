cef6c66d27baa1d5074070d5f0ea68f3d3a707f9
Anna Pawlik <pawlikanna24@gmail.com>
2021-02-06 12:01:41 +0100

    Add tailwind css

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import 'animate.css/animate.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import './assets/main.css';
 
 const Root = () => (
     <Provider store={store}>
---
+++ client/tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  important: true,
+  purge: [],
+  darkMode: false,
+  theme: {
+    fontFamily: {
+        body: ['Gotham Book', 'sans-serif'],
+        title: ['Butler', 'sans-serif'],
+    },
+    extend: {
+        colors: {
+             nav:  {
+                100: '#3B6780',
+              },
+              section: {
+                100: '#5E697E',
+                200: '#EEE1D9',
+                300: '#F6F1ED',
+                400: '#37776d',
+                500: '#F4F4F4',
+                600: '#0E3E01',
+              },
+        },
+        height: {
+          header: '40vh',
+          section: '10vh',
+        },
+        width: {
+          '1/3': '31.666667%',
+        }
+      },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
