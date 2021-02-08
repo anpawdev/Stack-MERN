8d3f90380a8c793a8d62f1826102c52ec53685a8
Anna Pawlik <pawlikanna24@gmail.com>
2021-02-08 17:52:31 +0100

    Change server config

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: 8000,
-    DB: 'mongodb+srv://User_app:testapp12blog@cluster0-lnujk.mongodb.net/test?retryWrites=true&w=majority',
+    DB: 'mongodb+srv://User_app:testappblog@cluster0.lnujk.mongodb.net/<dbname>?retryWrites=true&w=majority',
 };
