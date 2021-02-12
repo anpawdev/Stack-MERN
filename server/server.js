296c07ef867e6824b3d6442e09bc504df5eba2a7
Anna Pawlik <pawlikanna24@gmail.com>
2021-02-12 10:04:25 +0100

    Change config

--- server/server.js
+++ server/server.js
@@ -1,73 +1,24 @@
-// const express = require('express');
-// const cors = require('cors');
-// const config = require('./config');
-// const mongoose = require('mongoose');
-// const loadTestData = require('./testData');
-// const helmet = require('helmet');
-// const sanitize = require('mongo-sanitize');
-// const path = require('path');
-
-// const app = express();
-// const PORT = process.env.PORT || 8000;
-
-// // import routes
-// const postRoutes = require('./routes/post.routes');
-
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true}));
-// app.use(express.json());
-// app.use(express.json(sanitize()));
-// app.use('/api', postRoutes);
-// app.use(helmet());
-// // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, '/../client/build')));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-// });
-
-// // connects our back end code with the database
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/blogappmern', { useNewUrlParser: true });
-// let db = mongoose.connection;
-
-// db.once('open', () => {
-//   console.log('Connected to the database');
-//   loadTestData();
-// });
-// db.on('error', (err) => console.log('Error ' + err));
-
-// app.listen(PORT, function() {
-//   console.log('Server is running on Port:', PORT);
-// });
-
 const express = require('express');
 const cors = require('cors');
 const config = require('./config');
 const mongoose = require('mongoose');
+const loadTestData = require('./testData');
 const helmet = require('helmet');
-
-mongoose.set('useFindAndModify', false);
-
-const mongoSanitize = require('mongo-sanitize');
+const sanitize = require('mongo-sanitize');
 const path = require('path');
 
-const loadTestData = require('./testData');
-
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 // import routes
 const postRoutes = require('./routes/post.routes');
 
-app.use(helmet());
 app.use(cors());
-app.use(express.urlencoded({ extended: true })); // false?
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.json(sanitize()));
 app.use('/api', postRoutes);
-app.use((req, res, next) => {
-  mongoSanitize(req.body);
-  next();
-});
-
+app.use(helmet());
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/../client/build')));
 
@@ -76,15 +27,15 @@ app.get('*', (req, res) => {
 });
 
 // connects our back end code with the database
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/blogappmern', { useNewUrlParser: true });
 let db = mongoose.connection;
 
 db.once('open', () => {
   console.log('Connected to the database');
   loadTestData();
 });
-db.on('error', err => console.log('Error ' + err));
+db.on('error', (err) => console.log('Error ' + err));
 
-app.listen(config.PORT, function() {
-  console.log('Server is running on port:', config.PORT);
+app.listen(PORT, function() {
+  console.log('Server is running on Port:', PORT);
 });
